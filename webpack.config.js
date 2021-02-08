8df77a8a961b27d3be2cb3e52c6f3a5aabd92aaf
Akhilesh <akhilesh@ipedis.com>
2021-02-08 13:23:13 +0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+    entry: './src/javascript/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: ''
+                        }
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "postcss-loader"
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("sass")
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            outputPath: 'images'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff|woff2|ttf|otf|eot)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            outputPath: 'fonts'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin ({
+            filename: "bundle.css"
+        }),
+
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            files: [
+              '../index.html'
+            ],
+            server: { baseDir: ['dist']}
+        })
+    ],
+
+
+
+    mode: 'development'
+};
